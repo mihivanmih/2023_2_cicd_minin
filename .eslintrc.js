87c94be953a6611456a702e30d5ff1bc1292cd22
Gusb <mihivanmih@gmail.com>
2023-02-18 15:18:50 +0300

    ci cd 10

---
+++ .eslintrc.js
@@ -0,0 +1,64 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true
+    },
+    extends: [
+        'plugin:react/recommended',
+        'standard-with-typescript',
+        'plugin:i18next/recommended'
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: ['tsconfig.json']
+    },
+    plugins: [
+        'react',
+        'i18next'
+    ],
+    rules: {
+        indent: [2, 4, { MemberExpression: 1 }],
+        'react/react-in-jsx-scope': 'off',
+        'react/jsx-indent': [2, 4],
+        '@typescript-eslint/indent': [2, 4],
+        '@typescript-eslint/strict-boolean-expressions': 'off',
+        '@typescript-eslint/naming-convention': 'off',
+        '@typescript-eslint/ban-ts-comment': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/consistent-type-assertions': 'warn',
+        'i18next/no-literal-string': 'warn',
+        'react/display-name': 'off',
+        'linebreak-style': 0,
+        'additionalProperties': false,
+        '@typescript-eslint/prefer-includes': 'off',
+        '@typescript-eslint/consistent-type-imports': [
+            'error',
+            {
+                prefer: 'type-imports'
+            }
+        ],
+        'react/self-closing-comp': [
+            'error', {
+                component: true,
+                html: true
+            }
+        ]
+    },
+    globals: {
+        __IS_DEV__: true
+    },
+    settings: {
+        react: {
+            version: 'detect'
+        }
+    },
+    overrides: [
+        {
+            files: ['**/src/**/*.test.{ts,tsx}'],
+            rules: {
+                'i18next/no-literal-string': 'off'
+            }
+        }
+    ]
+}
